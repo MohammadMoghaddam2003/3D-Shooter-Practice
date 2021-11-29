b7bd6b2c757d0612202c12b908405690d6060b1c
MohammadMoghaddam2003 <m.moghaddam2003@gmail.com>
2021-11-29 00:41:37 -0800

    Change Capsule

--- Assets/Scripts/FPS Controller/FPSController.cs
+++ Assets/Scripts/FPS Controller/FPSController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class FPSController : MonoBehaviour
 {
     [Range(0f, 20f)]
-    public float WalkSpeed = 6.5f, RunSpeed = 10f, CrouchingWalkSpeed = 2f, JumpForce = 3f, CrouchingSpeed = 3f;
+    public float WalkSpeed = 6.5f, RunSpeed = 10f, CrouchingWalkSpeed = 2f, JumpForce = 3f, CrouchingSpeed = 3f
+    , GravityForce = 10f;
 
-    private Rigidbody _playerRigidbody;
-    private BoxCollider _boxColliderForCanJumping;
-    private CapsuleCollider _playerCapsuleCollider;
+    private CharacterController _playerCharacterController;
     private Transform _cameraTransform;
-    private bool _canJumping = false, _isCrouching;
+    private Animator _playerAnimator;
+    private Vector3 _moveDirection;
+    private bool _isGrounded = false, _isCrouching, _isWalking;
     private float _inputX = 0, _inputZ = 0, _inputModifyFactor, _speed, _playerCapsuleColliderDefaultHeight
     , _playerCapsuleColliderSetHeight, _cameraDefaultHeight, _cameraSetHeight;
 
@@ -22,23 +23,14 @@ public class FPSController : MonoBehaviour
         _speed = WalkSpeed;
         _cameraTransform = GameObject.Find("Main Camera").transform;
         _cameraDefaultHeight = _cameraTransform.localPosition.y;
-        _playerRigidbody = GetComponent<Rigidbody>();
-        _boxColliderForCanJumping = GetComponent<BoxCollider>();
-        _playerCapsuleCollider = GetComponent<CapsuleCollider>();
-        _playerCapsuleColliderDefaultHeight = _playerCapsuleCollider.height;
+        _playerAnimator = GetComponent<Animator>();
+        _playerCharacterController = GetComponent<CharacterController>();
+        _playerCapsuleColliderDefaultHeight = _playerCharacterController.height;
     }
 
     void Update()
     {
         PlayerMovement();
-    }
-
-    void OnTriggerEnter(Collider Ground)
-    {
-        if (Ground.CompareTag("Ground"))
-        {
-            _canJumping = true;
-        }
     }
 
     void PlayerMovement()
@@ -71,16 +63,34 @@ public class FPSController : MonoBehaviour
 
 
 
-        _inputX = Input.GetAxis("Horizontal") * _speed;
-        _inputZ = Input.GetAxis("Vertical") * _speed;
+        _inputX = Input.GetAxis("Horizontal");
+        _inputZ = Input.GetAxis("Vertical");
+
+
 
         _inputModifyFactor = (_inputX != 0 && _inputZ != 0) ? .5f : 1;
+        _moveDirection = new Vector3((_inputX * _inputModifyFactor) * Time.deltaTime, 0, (_inputZ * _inputModifyFactor) * Time.deltaTime);
+        _moveDirection -= new Vector3(0f, GravityForce * Time.deltaTime, 0f);
+        _moveDirection = transform.TransformDirection(_moveDirection * _speed);
+        _isGrounded = (_playerCharacterController.Move(_moveDirection) & CollisionFlags.Below) != 0f;
 
-        transform.Translate(new Vector3((_inputX * _inputModifyFactor) * Time.deltaTime, 0, (_inputZ * _inputModifyFactor) * Time.deltaTime));
 
 
+        if (_inputX != 0f || _inputZ != 0f)
+        {
+            _isWalking = true;
+        }
+        else
+        {
+            _isWalking = false;
+        }
 
-        if (Input.GetKeyDown(KeyCode.Space) && _canJumping && !_isCrouching)
+        _playerAnimator.SetFloat("Walking", _playerCharacterController.velocity.magnitude);
+
+        _playerAnimator.SetBool("IsWalking", _isWalking);
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded && !_isCrouching)
         {
             JumpingPlayer();
         }
@@ -88,7 +98,7 @@ public class FPSController : MonoBehaviour
 
     void JumpingPlayer()
     {
-        _playerRigidbody.velocity = new Vector3(0, JumpForce * Time.deltaTime, 0);
+        _moveDirection += new Vector3(0f, JumpForce * Time.deltaTime, 0f);
     }
 
     IEnumerator SetCrouching()
@@ -96,12 +106,13 @@ public class FPSController : MonoBehaviour
         _playerCapsuleColliderSetHeight = _isCrouching ? _playerCapsuleColliderDefaultHeight / 2 : _playerCapsuleColliderDefaultHeight;
         _cameraSetHeight = _isCrouching ? _cameraDefaultHeight / 2 : _cameraDefaultHeight;
 
-        while (Mathf.Abs(_playerCapsuleCollider.height - _playerCapsuleColliderSetHeight) > 0.0001f)
+        while (Mathf.Abs(_playerCharacterController.height - _playerCapsuleColliderSetHeight) > 0.0001f)
         {
-            _playerCapsuleCollider.height = Mathf.Lerp(_playerCapsuleCollider.height, _playerCapsuleColliderSetHeight, CrouchingSpeed * Time.deltaTime);
+            _playerCharacterController.height = Mathf.Lerp(_playerCharacterController.height, _playerCapsuleColliderSetHeight, CrouchingSpeed * Time.deltaTime);
             _cameraTransform.localPosition = Vector3.Lerp(_cameraTransform.localPosition, new Vector3(_cameraTransform.localPosition.x, _cameraSetHeight, _cameraTransform.localPosition.z), CrouchingSpeed * Time.deltaTime);
         }
 
         yield return null;
     }
 }
+
